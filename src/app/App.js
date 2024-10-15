6440b95248409c73b2ed319937db6276f256f37d
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-15 10:43:34 +0530

    routes path designed and create needed components

---
+++ src/app/App.js
@@ -0,0 +1,39 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+import ListUserPage from './components/ListUserPage';
+import AddUserPage from './components/AddUserPage';
+import EditUserPage from './components/EditUserPage';
+import ViewUserPage from './components/ViewUserPage';
+
+import { Container, Navbar , Nav } from 'react-bootstrap';
+
+
+export default function App() {
+  return <BrowserRouter>
+  
+  <div>
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand  href='/'> API Application </Navbar.Brand>
+        <Nav ClassName="float-right">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/add">Add New</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+    <Container className='pt-5'>
+      <Routes>
+        <Route index element={<ListUserPage/>}  />
+        <Route path="add" element={<AddUserPage/>}  />
+        <Route path="user/edit" element={<EditUserPage />}  />
+        <Route path="users" element={<ViewUserPage/>}  />
+      </Routes>
+    </Container>
+  </div>
+
+  </BrowserRouter>
+
+}
+
+
---
+++ src/app/components/AddUserPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AddUserPage() {
+  return (
+    <div>
+      AddUserPage
+    </div>
+  )
+}
