8eb1e1238477a46e9f15f7d1779c35ee40d8cbf5
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-17 10:23:53 +0530

    axios installed from npm and bring datas from server and implements to ListUsers Page design with map function

--- src/app/components/ListUsersPage.js
+++ src/app/components/ListUsersPage.js
@@ -1,37 +1,51 @@
-import React from 'react'
-import { Table, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function ListUserPage() {
-  return <Table striped bordered hover>
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>Alex</td>
-      
-        <td>
+  const [data, setData] = useState([]);
 
-          <Link to={"/users/"}>
-            <Button variant="primary" size="sm" >View</Button>
-          </Link>
-          {" "}
-          <Link to={"/users/edit"}>
-            <Button variant="primary" size="sm" >Edit</Button>
-          </Link>
-          {" "}
-          <Link>
-            <Button variant="danger" size="sm" >Delete</Button>
-          </Link>
+  // Fetch users when the component mounts
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await axios.get("http://localhost:8000/users");
+      setData(response.data);
+    };
 
-        </td>
-      </tr>
-    </tbody>
-  </Table>
+    fetchUsers();
+  }, []); // Empty dependency array means this runs once when the component mounts
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>USER ID</th>
+          <th>USER NAME</th>
+          <th>ACTIONS</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.map((user, index) => (
+            <tr key={index}> {/* Key prop on the <tr> */}
+              <td>{user.id}</td> {/* Use curly braces to access variables */}
+              <td>{user.name}</td>
+              <td>
+                <Link to={`/users/${user.id}`}>
+                  <Button variant="primary" size="sm">View</Button>
+                </Link>
+                {" "}
+                <Link to={`/users/edit/${user.id}`}>
+                  <Button variant="info" size="sm">Edit</Button>
+                </Link>
+                {" "}
+                <Button variant="danger" size="sm">Delete</Button>
+              </td>
+            </tr>
+          ))
+        }
+      </tbody>
+    </Table>
+  );
 }
