37b90e74d7e8b0cfc0772f549dbcae887cdb69fc
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-19 11:32:53 +0530

    Fetch and display user details on ViewUserPage component
    - Implemented API call to retrieve user information based on userId from URL
    - Added loading state while fetching data
    - Rendered user details in a Bootstrap Card and Table layout
    - Displayed user ID, name, email, age, and premium membership status

--- src/app/components/ViewUserPage.js
+++ src/app/components/ViewUserPage.js
@@ -1,32 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import { Card, Table } from 'react-bootstrap'
+import { useParams } from 'react-router-dom'
+
 
 export default function ViewUserPage() {
+  const {userId} = useParams();
+  const [user, setUser] = useState(undefined);
+
+  // Fetch users when the component mounts
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`http://localhost:8000/users/${userId}`);
+      setUser(response.data);
+    };
+
+    fetchUser();
+  }, [userId]);
+
+  if (!user) {
+    return <div>Loading...</div>; // Show loading message while fetching
+  }
+
   return <Card>
     <Table>
       <tbody>
         <tr>
           <td>User ID</td>
-          <td>1</td>
+          <td>{userId}</td>
         </tr>
         <tr>
           <td>Name</td>
-          <td>ajmal</td>
+          <td>{user.name}</td>
         </tr>
         <tr>
           <td>Email</td>
-          <td>ajmal@gmail.com</td>
+          <td>{user.email}</td>
         </tr>
         <tr>
           <td>Age</td>
-          <td>22</td>
+          <td>{user.age}</td>
         </tr>
         <tr>
           <td>Premium User</td>
-          <td>Yes</td>
+          <td>{
+            user.premiumMember? "yes" : "No"}</td>
         </tr>
       </tbody>
 
     </Table>
   </Card>
 }
+
