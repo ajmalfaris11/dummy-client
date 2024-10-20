3694a267357b03252369ed673394ba248461e22f
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-20 12:10:43 +0530

    Add AddUserPage component with form functionality
    - Created AddUserPage component with form fields for Name, Email, Age, and Premium User checkbox.
    - Implemented useState hooks to manage form state for each input.
    - Added responsive layout with Bootstrap classes and Card component for styling.
    - Adjusted checkbox handling to use checked attribute for accurate boolean control.
    - Corrected Age input type to 'number' for numerical input validation.

--- src/app/components/AddUserPage.js
+++ src/app/components/AddUserPage.js
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useState } from 'react';
+import { Card, Form, Button } from 'react-bootstrap';
 
 export default function AddUserPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [premiumMember, setPremiumMember] = useState(false);
+
   return (
-    <div>
-      AddUserPage
+    <div className="row justify-content-center">
+      <div className="col-md-4">
+        <Card className="p-4">
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Age</Form.Label>
+              <Form.Control
+                type="numbe"
+                placeholder="Age"
+                value={age}
+                onChange={(event) => setAge(event.target.value)}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Check
+                type="checkbox"
+                label="Premium User"
+                checked={premiumMember}
+                onChange={(event) => setPremiumMember(event.target.checked)}
+              />
+            </Form.Group>
+            
+            <Button variant="primary" type="submit">
+              Add New User
+            </Button>
+          </Form>
+        </Card>
+      </div>
     </div>
-  )
+  );
 }
