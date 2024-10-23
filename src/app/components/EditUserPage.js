9d03769a2cbcd61c46b42ecfc874ee103e426bca
Ajmal Faris <ajmalfaris.me@gmail.com>
2024-10-23 12:56:34 +0530

    Implement EditUserPage component with user detail fetching, form handling, and update functionality
    - Added useParams to get user ID from URL for targeted data fetching.
    - Integrated useNavigate for redirecting to the user's profile page after updating.
    - Implemented state management for form fields (name, email, age, and premium member status).
    - Added useEffect to fetch user details from API when component mounts and populate form fields.
    - Created updateUser function to handle form submission and send PATCH request to API with updated user data.
    - Enhanced form with validation for required fields and controlled inputs for real-time updates.

--- src/app/components/EditUserPage.js
+++ src/app/components/EditUserPage.js
@@ -1,28 +1,101 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Card, Form, Button } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+
+import { useNavigate } from 'react-router-dom'; // this hook using for navigate the page
+
 
 export default function EditUserPage() {
+  const navigate = useNavigate();
+  const {userId} = useParams();
+
+ const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
+  const [premiumMember, setPremiumMember] = useState(false);
+
+  // Fetch users when the component mounts
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await axios.get(`http://localhost:8000/users/${userId}`);
+      // setUser(response.data);
+
+      
+    const user = response.data;
+    setName(user.name);
+    setEmail(user.email);
+    setAge(user.age);
+    setPremiumMember(user.premiumMember);
+
+    };
+
+    fetchUser();
+  }, [userId]);
+
+  const updateUser = async (event) => {
+    event.preventDefault();
+    await axios.patch(
+      `http://localhost:8000/users/${userId}`,
+      {
+        name: name,
+        email: email,
+        age: parseInt(age),
+        premiumMember: premiumMember
+      }
+    );
+
+    navigate(`/users/${userId}`); // navigate to the user page
+
+
+  }
+
+
   return (
     <div className="row justify-content-center">
       <div className="col-md-4">
         <Card className="p-4">
-          <Form>
-            <Form.Group className="mb-3">
+          <Form onSubmit={updateUser}>
+          <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="name" placeholder="Enter name" />
+              <Form.Control
+                type="name"
+                required
+                placeholder="Enter name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Control
+                type="email"
+                required
+                placeholder="name@example.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3">
               <Form.Label>Age</Form.Label>
-              <Form.Control type="text" placeholder="Age" />
+              <Form.Control
+                type="number"
+                required
+                placeholder="Age"
+                value={age}
+                onChange={(event) => setAge(event.target.value)}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3">
-              <Form.Check type="checkbox" label="Premium User" />
+              <Form.Check
+                type="checkbox"
+                label="Premium User"
+                checked={premiumMember}
+                onChange={(event) => setPremiumMember(event.target.checked)}
+              />
             </Form.Group>
 
             <Button variant="info" type="submit">
